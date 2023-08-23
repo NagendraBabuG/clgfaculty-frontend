e7cf768c519531f8be6f6a4d01cf70eb2ddf1b4d
NagendraBabuG <gnagendra0009@gmail.com>
2023-08-23 22:14:13 +0530

    added all details in form  except image

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import axios from 'axios'; // Import Axios
 import './index.css'
 const FacultyUpdateForm = ({ user }) => {
@@ -9,12 +9,33 @@ const FacultyUpdateForm = ({ user }) => {
   const [contact, setContact] = useState(user.contact);
   const [educationList, setEducationList] = useState(user.education || []);
   const [publicationList, setPublicationList] = useState(user.publications || []);
-
+  const [areasOfInterest, setAreasOfInterest] = useState(user.areasOfInterest || null)
+  const [projects, setProjects] = useState(user.projects || [])
+  const [year, setJoinYear] = useState(user.year || '')
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email)
+      if(user.position) setPosition(user.position)
+      if(user.contact) setContact(user.contact)
+      if(user.education) setEducationList(user.education)
+      if(user.publications) setPublicationList(user.publications)
+      
+    }
+  }, [user]);
+  const handleAreasChange = (value) => {
+    setAreasOfInterest(value);
+  };
   const handleEducationChange = (index, field, value) => {
     const updatedEducationList = [...educationList];
     updatedEducationList[index][field] = value;
     setEducationList(updatedEducationList);
   };
+  const handleProjectChange = (index, field, value)=>{
+    const updatedProjectList = [...projects]
+    updatedProjectList[index][field] = value
+    setProjects(updatedProjectList)
+  }
 
   const handlePublicationChange = (index, field, value) => {
     const updatedPublicationList = [...publicationList];
@@ -25,9 +46,26 @@ const FacultyUpdateForm = ({ user }) => {
   const handleAddEducation = () => {
     setEducationList([...educationList, { institute: '', start: '', end: '', degree: '', remarks: '' }]);
   };
-
+  const handleAddProject = ()=>{
+    setProjects([...projects, {title: '', role : '', duration: '', status : '', sponsoredBy: ''}])
+  }
   const handleAddPublication = () => {
     setPublicationList([...publicationList, { title: '', date: '', remarks: '' }]);
+  };
+  const [course, setCourse] = useState('');
+  const [coursesList, setCoursesList] = useState(user?.coursesList || []);
+
+  const handleAddCourse = () => {
+    if (course.trim() !== '') {
+      setCoursesList([...coursesList, course]);
+      setCourse(''); // Clear the input field
+    }
+  };
+
+  const handleRemoveCourse = (index) => {
+    const updatedCourses = [...coursesList];
+    updatedCourses.splice(index, 1);
+    setCoursesList(updatedCourses);
   };
 
   const handleUpdate = (e) => {
@@ -55,7 +93,7 @@ const FacultyUpdateForm = ({ user }) => {
 
   return (
     <div>
-      <h2>Update Faculty Details</h2>
+      <h2>Update Details</h2>
       <form onSubmit={handleUpdate}>
         <label>Name:</label>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
@@ -77,7 +115,7 @@ const FacultyUpdateForm = ({ user }) => {
         {educationList.map((education, index) => (
           
           <div key={index}>
-            <div><h2>{`Education ${index + 1}`}</h2></div>
+            <div><h2>{`Education ${index + 1}`}</h2> </div>
             <input
               type="text"
               value={education.institute}
@@ -94,6 +132,32 @@ const FacultyUpdateForm = ({ user }) => {
           </div>
         ))}
         <button type="button" onClick={handleAddEducation}>Add Education</button>
+        
+        <h3>Projects:</h3>
+        {projects.map((project, index) => (
+          
+          <div key={index}>
+            <div><h2>{`Project ${index + 1}`}</h2> </div>
+            <input
+              type="text"
+              value={project.title}
+              onChange={(e) => handleProjectChange(index, 'title', e.target.value)}
+              placeholder="Title"
+            />
+            <input type='text' value = {project.sponsoredBy} onChange={(e)=>handleProjectChange(index, 'sponsoredBy', e.target.value)}
+            placeholder='SponsoredBy'/>
+            <input type='text' value={project.role} onChange={(e)=>handleProjectChange(index, 'role', e.target.value)} placeholder='Role'/>
+            <input type='text' value={project.duration} onChange={(e)=>handleProjectChange(index, 'duration', e.target.value)} placeholder='Duration'/>
+            <label>Position:</label>
+          <select value={project.status} onChange={(e) => handleProjectChange(index, 'status',e.target.value)}>
+            <option value="running">Running</option>
+            <option value="completed">Completed</option>
+          </select>
+            {/* ... other education fields */}
+          </div>
+        ))}
+        <button type="button" onClick={handleAddProject}>Add</button>
+
 
         <h3>Publications:</h3>
         {publicationList.map((publication, index) => (
@@ -108,6 +172,41 @@ const FacultyUpdateForm = ({ user }) => {
           </div>
         ))}
         <button type="button" onClick={handleAddPublication}>Add Publication</button>
+      
+        <div>
+          <label>Areas of Interest:</label>
+          <textarea
+            rows="3"
+            value={areasOfInterest}
+            onChange={(e) => handleAreasChange(e.target.value)}
+          />
+        </div>
+
+        <div>
+      <h2>Courses Taught</h2>
+      <div>
+        <label>Enter Course:</label>
+        <input
+          type="text"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        />
+        <button type="button" onClick={handleAddCourse}>Add Course</button>
+      </div>
+      <ul>
+        {coursesList.map((course, index) => (
+          <li key={index}>
+            {course}
+            <button type="button" onClick={() => handleRemoveCourse(index)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <label>Joined Year:</label>
+        <input type="text" value={year} onChange={(e) => setJoinYear(e.target.value)} />
 
         <button type="submit">Update</button>
       </form>
